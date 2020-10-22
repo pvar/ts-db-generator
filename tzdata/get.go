4493569b5d0db0b73eddf060e40923cbe7e6b619
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-22 13:37:07 +0300

    Make sure location parameter to GetData is never empty.

--- tzdata/get.go
+++ tzdata/get.go
@@ -5,6 +5,10 @@ import (
 )
 
 func GetData(location string) (*TZdata, error) {
+	if len(location) == 0 {
+		return nil, errors.New("tzdata: empty location name")
+	}
+
 	if containsDotDot(location) || location[0] == '/' || location[0] == '\\' {
 		// No valid IANA Time Zone name contains a single dot,
 		// much less dot dot. Likewise, none begin with a slash.
