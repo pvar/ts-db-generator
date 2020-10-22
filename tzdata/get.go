3ae44337f383364c1895ff53de9a4b617ebe9ac2
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-22 09:49:26 +0300

    Add simplified version of time.LoadLocation.
    Actually it's two functions: GetData and containsDotDot.
    The first uses the second to perform a basic sanity name
    on location name and then attempts to read the corresponding
    timezone file. GetData is a simplified version of LoadLocation.
    GetData assumes that it is running on Linux machines and that
    the input parameter is sanitized.

---
+++ tzdata/get.go
@@ -0,0 +1,32 @@
+package tzdata
+
+import (
+	"errors"
+)
+
+func GetData(location string) (*TZdata, error) {
+	if containsDotDot(location) || location[0] == '/' || location[0] == '\\' {
+		// No valid IANA Time Zone name contains a single dot,
+		// much less dot dot. Likewise, none begin with a slash.
+		return nil, errors.New("tzdata: invalid location name")
+	}
+
+	data, err := readTZfile(location)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func containsDotDot(s string) bool {
+	if len(s) < 2 {
+		return false
+	}
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == '.' && s[i+1] == '.' {
+			return true
+		}
+	}
+	return false
+}
