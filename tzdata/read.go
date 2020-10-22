c026a3ff915115350787da44960621c11b5ab1aa
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-22 10:32:56 +0300

    Add functions for actually loading timezone files.
    Once again, the implementation is shamelessly copied from functions
    in package time (zoneinfo_read.go). These functions are attempting
    to load the correct timezone file in memory and if successful,
    start the parsing.

---
+++ tzdata/read.go
@@ -0,0 +1,80 @@
+package tzdata
+
+import (
+        "errors"
+        "syscall"
+)
+
+// maxFileSize is the max permitted size of files read by readFile.
+// As reference, the zoneinfo.zip distributed by Go is ~350 KB,
+// so 10MB is overkill.
+const maxFileSize = 10 << 20
+
+// loadLocation returns the Location with the given name from one of
+// the specified sources. See loadTzinfo for a list of supported sources.
+// The first timezone data matching the given name that is successfully loaded
+// and parsed is returned as a Location.
+func readTZfile(name string) (z *TZdata, firstErr error) {
+        var file string
+
+        for _, source := range []string{"/usr/share/zoneinfo/", "/usr/share/lib/zoneinfo/"} {
+                file = source + "/" + name
+
+                var rawTZdata, err = loadFile(file)
+
+                if err == nil {
+                        if data, err := parseRawTZdata(name, rawTZdata); err == nil {
+                                return data, nil
+                        }
+                }
+        }
+        return nil, errors.New("unknown time zone " + name)
+}
+
+// loadFile reads and returns the content of the named file.
+// It is a trivial implementation of ioutil.ReadFile,
+// reimplemented to avoid depending on io/ioutil or os.
+// It returns an error if name exceeds maxFileSize bytes.
+func loadFile(name string) ([]byte, error) {
+        f, err := open(name)
+        if err != nil {
+                return nil, err
+        }
+        defer closefd(f)
+
+        var (
+                buf [4096]byte
+                ret []byte
+                n   int
+        )
+
+        for {
+                n, err = read(f, buf[:])
+                if n > 0 {
+                        ret = append(ret, buf[:n]...)
+                }
+                if n == 0 || err != nil {
+                        break
+                }
+                if len(ret) > maxFileSize {
+                        return nil, errors.New("tzdata: timezone file too big")
+                }
+        }
+        return ret, err
+}
+
+func open(name string) (uintptr, error) {
+        fd, err := syscall.Open(name, syscall.O_RDONLY, 0)
+        if err != nil {
+                return 0, err
+        }
+        return uintptr(fd), nil
+}
+
+func read(fd uintptr, buf []byte) (int, error) {
+        return syscall.Read(int(fd), buf)
+}
+
+func closefd(fd uintptr) {
+        syscall.Close(int(fd))
+}
