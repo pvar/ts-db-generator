3c036780fd49171b9b40ee875dbc7ffbed98e0e2
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-22 09:21:02 +0300

    Add structure for timezone data in tzdata package.
    tzdata package will copy functionality (and code) from time
    package. tzdata struct is a stripped down version of Location,
    that exposes all data related to the selected timezone.

---
+++ tzdata/tzdata.go
@@ -0,0 +1,33 @@
+package tzdata
+
+// TZdata collects time offsets and offset-transitions for a geographical area.
+// Typically, the TZdata struct represents the collection of time offsets
+// in use in a geographical area. For many Locations the time offset varies
+// depending on whether daylight savings time is in use.
+type TZdata struct {
+    Name  string
+    Eras  []Era
+    Trans []EraTrans
+
+    // The tzdata information can be followed by a string that describes
+    // how to handle DST transitions not recorded in zoneTrans.
+    // The format is the TZ environment variable without a colon;
+    // https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html.
+    Extend string
+}
+
+// A zone represents a single time zone (CET, CEST, etc).
+type Era struct {
+    Name   string // abbreviated name of zone
+    Offset int    // seconds east of UTC
+    IsDST  bool   // is this zone Daylight Savings Time?
+}
+
+// A zoneTrans represents a single time zone transition.
+type EraTrans struct {
+    When         int64 // transition time, in seconds since 1970 GMT
+    Index        uint8 // index of the zone that goes into effect at that time
+    Isstd, Isutc bool  // seems to be ignored
+    // supposed to indicate whether transition time (When)
+    // expressed in UTC or local
+}
