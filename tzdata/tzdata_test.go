989d66645b91e9021a2624850e10fc83b44cc214
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-22 13:48:18 +0300

    Apply standard code formatting.

--- tzdata/tzdata_test.go
+++ tzdata/tzdata_test.go
@@ -1,62 +1,61 @@
 package tzdata
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestBadLocations(t *testing.T) {
-    badLocations := []string{
-            "Atlantis/nonexistent",
-            "/Asia/Shanghai",
-            "Asia\\Shanghai",
-            "..Europe/Athens",
-            "Athens",
-            "Europe",
-            "..",
-            "/",
-            ""}
+	badLocations := []string{
+		"Atlantis/nonexistent",
+		"/Asia/Shanghai",
+		"Asia\\Shanghai",
+		"..Europe/Athens",
+		"Athens",
+		"Europe",
+		"..",
+		"/",
+		""}
 
-    for _, badLocation := range badLocations {
-        fmt.Printf("\nAttempt to get data for: %-24q Expecting some error... ", badLocation)
-        _, err := GetData(badLocation)
-        if err == nil {
-            t.Errorf("Did not get error for %q!", badLocation)
-        } else {
-            fmt.Printf("got it!")
-        }
-    }
-    fmt.Println("")
+	for _, badLocation := range badLocations {
+		fmt.Printf("\nAttempt to get data for: %-24q Expecting some error... ", badLocation)
+		_, err := GetData(badLocation)
+		if err == nil {
+			t.Errorf("Did not get error for %q!", badLocation)
+		} else {
+			fmt.Printf("got it!")
+		}
+	}
+	fmt.Println("")
 }
 
 func TestWierdLocations(t *testing.T) {
-    badLocations := []string{
-            "Asia/Shanghai",    // no DST
-            "Etc/GMT",          // no transitions
-            "Etc/GMT-14",       // large offset
-            "right/Portugal"}   // has leap seconds
+	badLocations := []string{
+		"Asia/Shanghai",  // no DST
+		"Etc/GMT",        // no transitions
+		"Etc/GMT-14",     // large offset
+		"right/Portugal"} // has leap seconds
 
-    for _, badLocation := range badLocations {
-        fmt.Printf("\nAttempt to get data for: %-24q Should not get any errors... ", badLocation)
-        _, err := GetData(badLocation)
-        if err != nil {
-            t.Errorf("Got error for %q: %s", badLocation, err)
-        } else {
-            fmt.Printf("all's good!")
-        }
-    }
-    fmt.Println("")
+	for _, badLocation := range badLocations {
+		fmt.Printf("\nAttempt to get data for: %-24q Should not get any errors... ", badLocation)
+		_, err := GetData(badLocation)
+		if err != nil {
+			t.Errorf("Got error for %q: %s", badLocation, err)
+		} else {
+			fmt.Printf("all's good!")
+		}
+	}
+	fmt.Println("")
 }
 
-
 func BenchmarkLargeTZfile(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        GetData("Europe/Belfast")
-    }
+	for i := 0; i < b.N; i++ {
+		GetData("Europe/Belfast")
+	}
 }
 
 func BenchmarkSmallTZfile(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        GetData("Zulu")
-    }
+	for i := 0; i < b.N; i++ {
+		GetData("Zulu")
+	}
 }
