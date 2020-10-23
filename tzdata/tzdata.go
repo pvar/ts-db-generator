15a13844d4c69cdb2ab7596408c931f42e7845fd
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-23 13:53:42 +0300

    Add more tests for TZ string parsing.

--- tzdata/tzdata.go
+++ tzdata/tzdata.go
@@ -12,8 +12,8 @@
 package tzdata
 
 import (
-	"fmt"
-	"time"
+        "fmt"
+        "time"
 )
 
 // TZdata collects time offsets and offset-transitions for a geographical area.
@@ -21,108 +21,100 @@ import (
 // in use in a geographical area. For many Locations the time offset varies
 // depending on whether daylight savings time is in use.
 type TZdata struct {
-	Name  string
-	Eras  []Era
-	Trans []EraTrans
+        Name  string
+        Eras  []Era
+        Trans []EraTrans
 
-	// The tzdata information can be followed by a string that describes
-	// how to handle DST transitions not recorded in zoneTrans.
-	// The format is the TZ environment variable without a colon;
-	// https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html.
-	Extend string
+        // The tzdata information can be followed by a string that describes
+        // how to handle DST transitions not recorded in zoneTrans.
+        // The format is the TZ environment variable without a colon;
+        // https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html.
+        Extend string
 }
 
 // A zone represents a single time zone (CET, CEST, etc).
 type Era struct {
-	Name   string // abbreviated name of zone
-	Offset int    // seconds east of UTC
-	IsDST  bool   // is this zone Daylight Savings Time?
+        Name   string // abbreviated name of zone
+        Offset int    // seconds east of UTC
+        IsDST  bool   // is this zone Daylight Savings Time?
 }
 
 // A zoneTrans represents a single time zone transition.
 type EraTrans struct {
-	When         int64 // transition time, in seconds since 1970 GMT
-	Index        uint8 // index of the zone that goes into effect at that time
-	Isstd, Isutc bool  // seems to be ignored
-	// supposed to indicate whether transition time (When)
-	// expressed in UTC or local
+        When         int64 // transition time, in seconds since 1970 GMT
+        Index        uint8 // index of the zone that goes into effect at that time
+        Isstd, Isutc bool  // seems to be ignored
+        // supposed to indicate whether transition time (When)
+        // expressed in UTC or local
 }
 
 func (tzd *TZdata) PrintRaw() {
-	fmt.Printf("\nRaw data for %q.\n", tzd.Name)
+        fmt.Printf("\nRaw data for %q.\n", tzd.Name)
 
-	fmt.Printf("    era names:\n")
-	for i, era := range tzd.Eras {
-		fmt.Printf("        [%v] name: %-5s offset: %-6v DST: %v\n",
-			i,
-			era.Name,
-			era.Offset,
-			era.IsDST)
-	}
-	fmt.Printf("    transitions:\n")
-	for i, trans := range tzd.Trans {
-		fmt.Printf("        [%v] era: (%v) %-6s unix time: %-12v {isstd: %v, isutc: %v}\n",
-			i,
-			trans.Index,
-			tzd.Eras[trans.Index].Name,
-			trans.When,
-			trans.Isstd,
-			trans.Isutc)
-	}
-	fmt.Printf("    TZ variable: %s\n", tzd.Extend)
+        fmt.Printf("    era names:\n")
+        for i, era := range tzd.Eras {
+                fmt.Printf("        [%v] name: %-5s offset: %-6v DST: %v\n",
+                        i,
+                        era.Name,
+                        era.Offset,
+                        era.IsDST)
+        }
+        fmt.Printf("    transitions:\n")
+        for i, trans := range tzd.Trans {
+                fmt.Printf("        [%v] era: (%v) %-6s unix time: %-12v {isstd: %v, isutc: %v}\n",
+                        i,
+                        trans.Index,
+                        tzd.Eras[trans.Index].Name,
+                        trans.When,
+                        trans.Isstd,
+                        trans.Isutc)
+        }
+        fmt.Printf("    TZ variable: %s\n", tzd.Extend)
 }
 
 func (tzd *TZdata) PrintProcessed() {
-	fmt.Printf("\nProcessed data for %s:\n", tzd.Name)
-	transitionsCount := len(tzd.Trans)
+        fmt.Printf("\nProcessed data for %s:\n", tzd.Name)
 
-	// If no transitions defined,
-	// consider the last entry in eras slice as the current era.
-	if transitionsCount == 0 {
-		fmt.Println("!! No transitions defined. Will pick last defined era...")
-		fmt.Printf("    current era: %s\n", tzd.Eras[len(tzd.Eras)-1].Name)
-		fmt.Printf("        offset : %v\n", tzd.Eras[len(tzd.Eras)-1].Offset)
-		fmt.Printf("        end    : unknown\n")
-		return
-	}
+        now := time.Now()
+        nowEpoch := now.Unix()
 
-	// Transitions are always sorted in ascending order...
-	now := time.Now()
-	nowEpoch := now.Unix()
-	foundTransition := false
-	nextTransIndex := 0
-	for ; nextTransIndex < transitionsCount; nextTransIndex++ {
-		if nowEpoch < tzd.Trans[nextTransIndex].When {
-			foundTransition = true
-			break
-		}
-	}
+        name, offset, start, end := tzd.Lookup(nowEpoch)
+        fmt.Printf("    Current Era: %q\n", name)
+        fmt.Printf("        Offset : %v\n", offset)
 
-	// If no transitions ahead,
-	// current era is defined by the last transition.
-	if !foundTransition {
-		fmt.Println("!! No transitions in foreseeable future. Last transition determines current era.")
-		fmt.Printf("    current era: %s\n", tzd.Eras[tzd.Trans[transitionsCount-1].Index].Name)
-		fmt.Printf("        offset : %v\n", tzd.Eras[tzd.Trans[transitionsCount-1].Index].Offset)
-		fmt.Printf("        end    : gnab gib :-)\n")
-		return
-	}
+        var t time.Time
+        if start == bigbang  || start == gnabgib {
+                fmt.Printf("        start  : bigbang\n")
+        } else {
+                t = time.Unix(start, 0)
+                fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
+        }
 
-	// The immediately previous transition specifies the name of current era.
-	// If the next transition is the first in the slice, there is no certan way
-	// to deduce the name of the current era.
-	if nextTransIndex == 0 {
-		fmt.Printf("!! No past transition found. Cannot deduce name of curent era. Aborting...")
-		return
-	}
+        if end == gnabgib {
+                fmt.Printf("        stop   : gnabgib\n")
+        } else {
+                t = time.Unix(end, 0)
+                fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
+        }
 
-	fmt.Printf("    current era: %s\n", tzd.Eras[tzd.Trans[nextTransIndex-1].Index].Name)
-	fmt.Printf("        offset : %v\n", tzd.Eras[tzd.Trans[nextTransIndex-1].Index].Offset)
-	end := time.Unix(tzd.Trans[nextTransIndex].When-1, 0)
-	fmt.Printf("        end    : (%v) %s\n", tzd.Trans[nextTransIndex].When-1, end.Format("Mon, 02 01 2006 15:04:05"))
+        if end < gnabgib {
+                name, offset, start, end = tzd.Lookup(end+1)
+                fmt.Printf("    Comming Era: %q\n", name)
+                fmt.Printf("        Offset : %v\n", offset)
 
-	fmt.Printf("    comming era: %s\n", tzd.Eras[tzd.Trans[nextTransIndex].Index].Name)
-	fmt.Printf("        offset : %v\n", tzd.Eras[tzd.Trans[nextTransIndex].Index].Offset)
-	start := time.Unix(tzd.Trans[nextTransIndex].When, 0)
-	fmt.Printf("        start  : (%v) %s\n", tzd.Trans[nextTransIndex].When, start.Format("Mon, 02 01 2006 15:04:05"))
+                var t time.Time
+                if start == bigbang  || start == gnabgib {
+                        fmt.Printf("        start  : bigbang\n")
+                } else {
+                        t = time.Unix(start, 0)
+                        fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
+                }
+
+                if end == gnabgib {
+                        fmt.Printf("        stop   : gnabgib\n")
+                } else {
+                        t = time.Unix(end, 0)
+                        fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
+                }
+        }
 }
--- tzdata/tzdata_test.go
+++ tzdata/tzdata_test.go
@@ -18,16 +18,12 @@ func TestBadLocations(t *testing.T) {
                 ""}
 
         for _, badLocation := range badLocations {
-                fmt.Printf("\nAttempt to get data for: %-24q Expecting error... ", badLocation)
                 _, err := GetData(badLocation)
                 if err == nil {
-                        fmt.Printf("no error!!")
+                        fmt.Printf("\nAttempt to get data for %-24q should produce an error, but it did not!", badLocation)
                         t.Errorf("Did not get error for %q!", badLocation)
-                } else {
-                        fmt.Printf("error occured")
                 }
         }
-        fmt.Println("")
 }
 
 func TestWierdLocations(t *testing.T) {
@@ -38,16 +34,12 @@ func TestWierdLocations(t *testing.T) {
                 "right/Portugal"} // has leap seconds
 
         for _, badLocation := range badLocations {
-                fmt.Printf("\nAttempt to get data for: %-24q Should get no errors... ", badLocation)
                 _, err := GetData(badLocation)
                 if err != nil {
-                        fmt.Printf("got error!!")
+                        fmt.Printf("\nAttempt to get data for %-24q should get no errors, but it did!", badLocation)
                         t.Errorf("Got error for %q: %s", badLocation, err)
-                } else {
-                        fmt.Printf("no error")
                 }
         }
-        fmt.Println("")
 }
 
 func TestMalformedTZData(t *testing.T) {
@@ -56,6 +48,77 @@ func TestMalformedTZData(t *testing.T) {
         _, err := parseRawTZdata("abc", []byte(issue29437))
         if err == nil {
                 t.Error("expected error, got none")
+        }
+}
+
+func TestTzset(t *testing.T) {
+        for _, test := range []struct {
+                inStr string
+                inEnd int64
+                inSec int64
+                name  string
+                off   int
+                start int64
+                end   int64
+                ok    bool
+        }{
+                {"", 0, 0, "", 0, 0, 0, false},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2159200800, "PDT", -7 * 60 * 60, 2152173600, 2172733200, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2152173599, "PST", -8 * 60 * 60, 2145916800, 2152173600, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2152173600, "PDT", -7 * 60 * 60, 2152173600, 2172733200, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2152173601, "PDT", -7 * 60 * 60, 2152173600, 2172733200, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2172733199, "PDT", -7 * 60 * 60, 2152173600, 2172733200, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2172733200, "PST", -8 * 60 * 60, 2172733200, 2177452800, true},
+                {"PST8PDT,M3.2.0,M11.1.0", 0, 2172733201, "PST", -8 * 60 * 60, 2172733200, 2177452800, true},
+        } {
+                name, off, start, end, ok := tzset(test.inStr, test.inEnd, test.inSec)
+                if name != test.name || off != test.off || start != test.start || end != test.end || ok != test.ok {
+                        t.Errorf("tzset(%q, %d, %d) = %q, %d, %d, %d, %t, want %q, %d, %d, %d, %t", test.inStr, test.inEnd, test.inSec, name, off, start, end, ok, test.name, test.off, test.start, test.end, test.ok)
+                }
+        }
+}
+
+func TestTzsetName(t *testing.T) {
+        for _, test := range []struct {
+                in   string
+                name string
+                out  string
+                ok   bool
+        }{
+                {"", "", "", false},
+                {"X", "", "", false},
+                {"PST", "PST", "", true},
+                {"PST8PDT", "PST", "8PDT", true},
+                {"PST-08", "PST", "-08", true},
+                {"<A+B>+08", "A+B", "+08", true},
+        } {
+                name, out, ok := tzsetName(test.in)
+                if name != test.name || out != test.out || ok != test.ok {
+                        t.Errorf("tzsetName(%q) = %q, %q, %t, want %q, %q, %t", test.in, name, out, ok, test.name, test.out, test.ok)
+                }
+        }
+}
+
+func TestTzsetOffset(t *testing.T) {
+        for _, test := range []struct {
+                in  string
+                off int
+                out string
+                ok  bool
+        }{
+                {"", 0, "", false},
+                {"X", 0, "", false},
+                {"+", 0, "", false},
+                {"+08", 8 * 60 * 60, "", true},
+                {"-01:02:03", -1*60*60 - 2*60 - 3, "", true},
+                {"01", 1 * 60 * 60, "", true},
+                {"100", 0, "", false},
+                {"8PDT", 8 * 60 * 60, "PDT", true},
+        } {
+                off, out, ok := tzsetOffset(test.in)
+                if off != test.off || out != test.out || ok != test.ok {
+                        t.Errorf("tzsetName(%q) = %d, %q, %t, want %d, %q, %t", test.in, off, out, ok, test.off, test.out, test.ok)
+                }
         }
 }
 
