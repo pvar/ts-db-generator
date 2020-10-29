bb56545539e1438d221eca022388948d0da3bd31
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-29 11:16:52 +0200

    Test AddReplicas function.

--- tzdbio/tzdbio_test.go
+++ tzdbio/tzdbio_test.go
@@ -7,11 +7,10 @@ import (
 
 func TestAddOriginal(t *testing.T) {
 
-        originals := []string{
+    originals := []string{
                 "Shanghai",
                 "Europe/Athens",
-                "Europe/Berlin",
-                "Portugal"}
+                "Europe/Berlin"}
 
         Open ("./testdb.sqlite")
         for _, original := range originals {
@@ -21,6 +20,30 @@ func TestAddOriginal(t *testing.T) {
                         t.Errorf("%s", err)
                 } else {
                         fmt.Printf("Original %q added with ID %v\n", original, id)
+                }
+        }
+}
+
+func TestAddReplica(t *testing.T) {
+        replicas := []struct{replica, original string} {
+                {"Beijing", "Shanghai"},
+                {"Shenzhen", "Shanghai"},
+                {"Tianjin", "Shanghai"},
+                {"Larisa", "Europe/Athens"},
+                {"Farsala", "Europe/Athens"},
+                {"Xalkida", "Europe/Athens"},
+                {"Frankfurt", "Europe/Berlin"},
+                {"Nurnberg", "Europe/Berlin"},
+                {"Hanover", "Europe/Berlin"}}
+
+        Open ("./testdb.sqlite")
+        for _, replica := range replicas {
+                err := AddReplicas([]string{replica.replica}, replica.original)
+                if err != nil {
+                        fmt.Printf("Failed to add replica %q with original %q\n", replica.replica, replica.original)
+                        t.Errorf("%s", err)
+                } else {
+                        fmt.Printf("Added replica %q with original %q\n", replica.replica, replica.original)
                 }
         }
 }
