b944f4cc5e85fd2d0ad4eab4d560a135a08612ed
Varelas Panagiotis <varelaspanos@gmail.com>
2020-10-30 10:57:55 +0200

    Move tzdata-printing functions to separate file.

---
+++ tzdata/print.go
@@ -0,0 +1,72 @@
+package tzdata
+
+func (tzd *TZdata) PrintRaw() {
+        fmt.Printf("\nRaw data for %q.\n", tzd.Name)
+
+        fmt.Printf("    era names:\n")
+        for i, era := range tzd.Eras {
+                fmt.Printf("        [%v] name: %-5s offset: %-6v DST: %v\n",
+                        i,
+                        era.Name,
+                        era.Offset,
+                        era.IsDST)
+        }
+        fmt.Printf("    transitions:\n")
+        for i, trans := range tzd.Trans {
+                fmt.Printf("        [%v] era: (%v) %-6s unix time: %-12v {isstd: %v, isutc: %v}\n",
+                        i,
+                        trans.Index,
+                        tzd.Eras[trans.Index].Name,
+                        trans.When,
+                        trans.Isstd,
+                        trans.Isutc)
+        }
+        fmt.Printf("    TZ variable: %s\n", tzd.Extend)
+}
+
+func (tzd *TZdata) PrintProcessed() {
+        fmt.Printf("\nProcessed data for %s:\n", tzd.Name)
+
+        now := time.Now()
+        nowEpoch := now.Unix()
+
+        name, offset, start, end := tzd.Lookup(nowEpoch)
+        fmt.Printf("    Current Era: %q\n", name)
+        fmt.Printf("        Offset : %v\n", offset)
+
+        var t time.Time
+        if start == bigbang  || start == gnabgib {
+                fmt.Printf("        start  : bigbang\n")
+        } else {
+                t = time.Unix(start, 0)
+                fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
+        }
+
+        if end == gnabgib {
+                fmt.Printf("        stop   : gnabgib\n")
+        } else {
+                t = time.Unix(end, 0)
+                fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
+        }
+
+        if end < gnabgib {
+                name, offset, start, end = tzd.Lookup(end+1)
+                fmt.Printf("    Comming Era: %q\n", name)
+                fmt.Printf("        Offset : %v\n", offset)
+
+                var t time.Time
+                if start == bigbang  || start == gnabgib {
+                        fmt.Printf("        start  : bigbang\n")
+                } else {
+                        t = time.Unix(start, 0)
+                        fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
+                }
+
+                if end == gnabgib {
+                        fmt.Printf("        stop   : gnabgib\n")
+                } else {
+                        t = time.Unix(end, 0)
+                        fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
+                }
+        }
+}
--- tzdata/tzdata.go
+++ tzdata/tzdata.go
@@ -16,6 +16,8 @@ import (
         "time"
 )
 
+const source_path string = "/usr/share/zoneinfo/"
+
 // TZdata collects time offsets and offset-transitions for a geographical area.
 // Typically, the TZdata struct represents the collection of time offsets
 // in use in a geographical area. For many Locations the time offset varies
@@ -46,75 +48,4 @@ type EraTrans struct {
         Isstd, Isutc bool  // seems to be ignored
         // supposed to indicate whether transition time (When)
         // expressed in UTC or local
-}
-
-func (tzd *TZdata) PrintRaw() {
-        fmt.Printf("\nRaw data for %q.\n", tzd.Name)
-
-        fmt.Printf("    era names:\n")
-        for i, era := range tzd.Eras {
-                fmt.Printf("        [%v] name: %-5s offset: %-6v DST: %v\n",
-                        i,
-                        era.Name,
-                        era.Offset,
-                        era.IsDST)
-        }
-        fmt.Printf("    transitions:\n")
-        for i, trans := range tzd.Trans {
-                fmt.Printf("        [%v] era: (%v) %-6s unix time: %-12v {isstd: %v, isutc: %v}\n",
-                        i,
-                        trans.Index,
-                        tzd.Eras[trans.Index].Name,
-                        trans.When,
-                        trans.Isstd,
-                        trans.Isutc)
-        }
-        fmt.Printf("    TZ variable: %s\n", tzd.Extend)
-}
-
-func (tzd *TZdata) PrintProcessed() {
-        fmt.Printf("\nProcessed data for %s:\n", tzd.Name)
-
-        now := time.Now()
-        nowEpoch := now.Unix()
-
-        name, offset, start, end := tzd.Lookup(nowEpoch)
-        fmt.Printf("    Current Era: %q\n", name)
-        fmt.Printf("        Offset : %v\n", offset)
-
-        var t time.Time
-        if start == bigbang  || start == gnabgib {
-                fmt.Printf("        start  : bigbang\n")
-        } else {
-                t = time.Unix(start, 0)
-                fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
-        }
-
-        if end == gnabgib {
-                fmt.Printf("        stop   : gnabgib\n")
-        } else {
-                t = time.Unix(end, 0)
-                fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
-        }
-
-        if end < gnabgib {
-                name, offset, start, end = tzd.Lookup(end+1)
-                fmt.Printf("    Comming Era: %q\n", name)
-                fmt.Printf("        Offset : %v\n", offset)
-
-                var t time.Time
-                if start == bigbang  || start == gnabgib {
-                        fmt.Printf("        start  : bigbang\n")
-                } else {
-                        t = time.Unix(start, 0)
-                        fmt.Printf("        start  : %s -- %v\n", t.Format(time.ANSIC), start)
-                }
-
-                if end == gnabgib {
-                        fmt.Printf("        stop   : gnabgib\n")
-                } else {
-                        t = time.Unix(end, 0)
-                        fmt.Printf("        stop   : %s -- %v\n", t.Format(time.ANSIC), end)
-                }
-        }
 }
